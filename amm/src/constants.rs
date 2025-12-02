6c7ff0003869461b2590fa58d066d7fa19192b5f
Chelioso <cheliosooo26@gmail.com>
2025-12-02 08:43:07 +0400

    fix typo

--- amm/src/constants.rs
+++ amm/src/constants.rs
@@ -4,7 +4,7 @@ pub const PROGRAM_ID: Pubkey = pubkey!("save8RQVPMWNTzU18t3GBvBkN9hT7jsGjiCQ28Fp
 pub const USD_STAR_MINT: Pubkey = pubkey!("star9agSpjiFe3M49B3RniVU4CMBBEK3Qnaqn3RGiFM");
 pub const USDC_MINT: Pubkey = pubkey!("EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v");
 
-pub const USD_STAR_BANK: Pubkey = pubkey!("save8RQVPMWNTzU18t3GBvBkN9hT7jsGjiCQ28FpD9H");
+pub const USD_STAR_BANK: Pubkey = pubkey!("sM6P4mh53CnG4faN4Fo3seY7wMSAiHdy8o6gKjwQF7A");
 pub const MAIN_USDC_VAULT: Pubkey = pubkey!("3bZ1qY6wfzyDH7QMPiRKLr6k8p1asdtyjvJyJsJBdv23");
 
 pub const MARGINFI_PROGRAM_ID: Pubkey = pubkey!("MFv2hWf31Z9kbCa1snEPYctwafyhdvnV7FZnsebVacA");
